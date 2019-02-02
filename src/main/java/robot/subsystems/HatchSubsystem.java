260b25daf797303e4e4446634abe71498ccf0dd7
Ethan <sbont@10.17.192.138>
2019-02-02 11:33:25 -0500

    Hatch subsystem start

--- src/main/java/robot/subsystems/HatchSubsystem.java
+++ src/main/java/robot/subsystems/HatchSubsystem.java
@@ -5,24 +5,32 @@ import com.torontocodingcollective.subsystem.TSubsystem;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.commands.pneumatics.DefaultPneumaticsCommand;
+import com.torontocodingcollective.speedcontroller.TSpeedController;
 import com.torontocodingcollective.speedcontroller.TCanSpeedController;
+import robot.RobotMap;
+import robot.commands.hatch.HorizontalSlideCommand;
 
 /**
  * Subsystem for the hatch mechanism. Involves the belt slider and pneumatic placement/grabbing mechanisim.
  */
 public class HatchSubsystem extends TSubsystem {
 
+    TSpeedController slideMotor = new TCanSpeedController(RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_TYPE,RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_ADDRESS);
 
     public void init() {
 
     };
 
-    @Override
     protected void initDefaultCommand() {
-        
+        setDefaultCommand(new HorizontalSlideCommand());
     }
 
-    public void updatePeriodic() {
+    public void setSlideSpeed (double slideSpeed){
+        slideMotor.set(slideSpeed);
+    }
 
+
+    public void updatePeriodic() {
+        SmartDashboard.putNumber("Slide Motor", slideMotor.get());
     }
 }
