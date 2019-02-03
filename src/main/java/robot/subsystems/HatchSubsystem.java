1e8343c4b0858ed4eaa5de396941d962c6dc39d9
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-02-03 09:52:39 -0500

    Create Hatch subsystem

--- src/main/java/robot/subsystems/HatchSubsystem.java
+++ src/main/java/robot/subsystems/HatchSubsystem.java
@@ -1,28 +1,27 @@
 package robot.subsystems;
 
+import com.torontocodingcollective.speedcontroller.TCanSpeedController;
+import com.torontocodingcollective.speedcontroller.TSpeedController;
 import com.torontocodingcollective.subsystem.TSubsystem;
 
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import robot.commands.pneumatics.DefaultPneumaticsCommand;
-import com.torontocodingcollective.speedcontroller.TSpeedController;
-import com.torontocodingcollective.speedcontroller.TCanSpeedController;
 import robot.RobotMap;
-import robot.commands.hatch.HorizontalSlideCommand;
+import robot.commands.hatch.DefaultHatchCommand;
 
 /**
  * Subsystem for the hatch mechanism. Involves the belt slider and pneumatic placement/grabbing mechanisim.
  */
 public class HatchSubsystem extends TSubsystem {
 
-    TSpeedController slideMotor = new TCanSpeedController(RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_TYPE,RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_ADDRESS);
+    TSpeedController slideMotor = new TCanSpeedController(
+    		RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_TYPE,RobotMap.HATCH_SLIDE_CAN_SPEED_CONTROLLER_ADDRESS);
 
     public void init() {
 
     }
 
     protected void initDefaultCommand() {
-        setDefaultCommand(new HorizontalSlideCommand());
+        setDefaultCommand(new DefaultHatchCommand());
     }
 
     public void setSlideSpeed (double slideSpeed) {
