5ffe989615aee59ecc2c7a011a3535a05880cce9
Ethan <sbont@10.17.192.138>
2019-02-05 14:27:34 -0500

    Work on hatch command, fixing.

--- src/main/java/robot/commands/hatch/DefaultHatchCommand.java
+++ src/main/java/robot/commands/hatch/DefaultHatchCommand.java
@@ -6,49 +6,56 @@ import com.torontocodingcollective.commands.TSafeCommand;
 import robot.Robot;
 
 /**
- *
+ * The default hatch command TODO: commenting
  */
 public class DefaultHatchCommand extends TSafeCommand {
 
-    private static final String COMMAND_NAME = 
-            DefaultHatchCommand.class.getSimpleName();
-    
-    public DefaultHatchCommand() {
-        
-        super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
-        
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.hatchSubsystem);
-    }
+	private static final String COMMAND_NAME = 
+			DefaultHatchCommand.class.getSimpleName();
 
-    @Override
-    protected String getCommandName() { return COMMAND_NAME; }
-    
-    @Override
-    protected String getParmDesc() { 
-        return super.getParmDesc(); 
-    }
+	public DefaultHatchCommand() {
 
-    // Called just before this Command runs the first time
-    @Override
-    protected void initialize() {
-        // Print the command parameters if this is the current
-        // called command (it was not sub-classed)
-        if (getCommandName().equals(COMMAND_NAME)) {
-            logMessage(getParmDesc() + " starting");
-        }
-    }
+		super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-    	//FIXME
-    }
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.hatchSubsystem);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+	@Override
+	protected String getCommandName() { return COMMAND_NAME; }
+
+	@Override
+	protected String getParmDesc() { 
+		return super.getParmDesc(); 
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		// Print the command parameters if this is the current
+		// called command (it was not sub-classed)
+		if (getCommandName().equals(COMMAND_NAME)) {
+			logMessage(getParmDesc() + " starting");
+		}
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		//FIXME left will ovveride right for now.
+		if (Robot.oi.getHatchSlideLeft()>0) {
+			Robot.hatchSubsystem.setSlideSpeed(Robot.oi.getHatchSlideLeft());
+		}
+		else if (Robot.oi.getHatchSlideRight()>0) {
+			Robot.hatchSubsystem.setSlideSpeed(Robot.oi.getHatchSlideRight());
+		}
+
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
 }
