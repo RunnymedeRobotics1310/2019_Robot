32796f734b4180591b87793798a69c8ea2bb7f36
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-02-06 18:41:00 -0500

    Update the defaultCargoCommand

--- src/main/java/robot/commands/cargo/DefaultCargoCommand.java
+++ src/main/java/robot/commands/cargo/DefaultCargoCommand.java
@@ -10,62 +10,54 @@ import robot.Robot;
  */
 public class DefaultCargoCommand extends TSafeCommand {
 
-    private static final String COMMAND_NAME = 
-            DefaultCargoCommand.class.getSimpleName();
-    
-    public DefaultCargoCommand() {
-        
-        super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
-        
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.cargoSubsystem);
-    }
+	private static final String COMMAND_NAME = 
+			DefaultCargoCommand.class.getSimpleName();
 
-    @Override
-    protected String getCommandName() { return COMMAND_NAME; }
-    
-    @Override
-    protected String getParmDesc() { 
-        return super.getParmDesc(); 
-    }
+	public DefaultCargoCommand() {
 
-    // Called just before this Command runs the first time
-    @Override
-    protected void initialize() {
-        // Print the command parameters if this is the current
-        // called command (it was not sub-classed)
-        if (getCommandName().equals(COMMAND_NAME)) {
-            logMessage(getParmDesc() + " starting");
-        }
-    }
+		super(TConst.NO_COMMAND_TIMEOUT, Robot.oi);
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.cargoSubsystem);
+	}
 
-        if (Robot.oi.getArmUp() > 0) {
-            if (Robot.cargoSubsystem.armUpLimitDetected()) {
-        		Robot.cargoSubsystem.setArmSpeed(0);
-        	} else {
-        		Robot.cargoSubsystem.setArmSpeed(Robot.oi.getArmUp()) ;
-        	}
-            
-        } else if (Robot.oi.getArmDown() > 0) {
-        	if (Robot.cargoSubsystem.armDownLimitDetected()) {
-        		Robot.cargoSubsystem.setArmSpeed(0);
-        	} else {
-        		Robot.cargoSubsystem.setArmSpeed(-Robot.oi.getArmDown()) ;
-        	}
-        
-        } else {
-            Robot.cargoSubsystem.setArmSpeed(0);
-        }
-    }
+	@Override
+	protected String getCommandName() { return COMMAND_NAME; }
 
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+	@Override
+	protected String getParmDesc() { 
+		return super.getParmDesc(); 
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		// Print the command parameters if this is the current
+		// called command (it was not sub-classed)
+		if (getCommandName().equals(COMMAND_NAME)) {
+			logMessage(getParmDesc() + " starting");
+		}
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+
+		if (Robot.oi.getArmUp() > 0) {
+			Robot.cargoSubsystem.setArmSpeed(Robot.oi.getArmUp()) ;
+
+		} else if (Robot.oi.getArmDown() > 0) {
+			Robot.cargoSubsystem.setArmSpeed(-Robot.oi.getArmDown()) ;
+
+		} else {
+			Robot.cargoSubsystem.setArmSpeed(0);
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
 }
