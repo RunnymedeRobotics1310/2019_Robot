6f192e9e19ad9a2649594295f1c80d6f532a510b
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-03-07 18:31:26 -0500

    Camera naming and resolution

--- src/main/java/robot/subsystems/CameraSubsystem.java
+++ src/main/java/robot/subsystems/CameraSubsystem.java
@@ -2,7 +2,9 @@ package robot.subsystems;
 
 import com.torontocodingcollective.subsystem.TSubsystem;
 
+import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.cameraserver.CameraServer;
 
 /**
@@ -12,8 +14,12 @@ public class CameraSubsystem extends TSubsystem {
 
     public CameraSubsystem() {
         //Uncomment this line to start a USB camera feed
-        UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture();
-        UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture();
+    	MjpegServer switchedCamera = CameraServer.getInstance().addServer("Switched");  // Port 1181
+        UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture("Hatch", 0); // Port 1182
+        camera1.setVideoMode(PixelFormat.kMJPEG, 320, 240, 15);
+        UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture("Cargo", 1); // Port 1183
+        
+        switchedCamera.setSource(camera1);
     }
 
     @Override
