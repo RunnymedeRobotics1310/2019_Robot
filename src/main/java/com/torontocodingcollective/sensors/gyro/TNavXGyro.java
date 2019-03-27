7ffed08a3af396ff6b425eb5805f8a3ed9799add
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-03-27 19:09:23 -0400

    Restore original version

--- src/main/java/com/torontocodingcollective/sensors/gyro/TNavXGyro.java
+++ src/main/java/com/torontocodingcollective/sensors/gyro/TNavXGyro.java
@@ -3,14 +3,10 @@ package com.torontocodingcollective.sensors.gyro;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.SPI.Port;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TNavXGyro extends TGyro {
 
-	public enum GyroAxis {YAW, PITCH, ROLL };
-	
     private final AHRS navXGyro;
-    private GyroAxis curAxis;
 
     public TNavXGyro() {
         this(false);
@@ -28,9 +24,6 @@ public class TNavXGyro extends TGyro {
 
     @Override
     public double getAngle() {
-    	SmartDashboard.putNumber("NavX X",   navXGyro.getRawGyroX());
-    	SmartDashboard.putNumber("NavX Y",   navXGyro.getRawGyroY());
-    	SmartDashboard.putNumber("NavX Z",   navXGyro.getRawGyroZ());
         return super.getAngle(navXGyro.getAngle());
     }
 
