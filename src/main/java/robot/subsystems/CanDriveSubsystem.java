daedfc06a6664db7b292fbf8fad9c11c6b486eed
Richard McMullin <richard.e.mcmullin@gmail.com>
2019-03-27 18:23:37 -0400

    NavXGyro on prod bot

--- src/main/java/robot/subsystems/CanDriveSubsystem.java
+++ src/main/java/robot/subsystems/CanDriveSubsystem.java
@@ -1,7 +1,7 @@
 package robot.subsystems;
 
 import com.torontocodingcollective.sensors.encoder.TEncoder;
-import com.torontocodingcollective.sensors.gyro.TAnalogGyro;
+import com.torontocodingcollective.sensors.gyro.TNavXGyro;
 import com.torontocodingcollective.sensors.ultrasonic.TUltrasonicSensor;
 import com.torontocodingcollective.speedcontroller.TCanSpeedController;
 import com.torontocodingcollective.subsystem.TGyroDriveSubsystem;
@@ -20,115 +20,115 @@ import robot.commands.drive.DefaultDriveCommand;
 public class CanDriveSubsystem extends TGyroDriveSubsystem {
 
 	TUltrasonicSensor ultrasonicSensor = new TUltrasonicSensor(RobotMap.ULTRASONIC_ANALOG_PORT);
-	
-    public CanDriveSubsystem() {
 
-        super(
-                // Left Speed Controller
-                new TCanSpeedController(
-                       RobotMap.LEFT_DRIVE_CAN_SPEED_CONTROLLER_TYPE,
-                      RobotMap.LEFT_DRIVE_CAN_SPEED_CONTROLLER_ADDRESS,
-                       RobotMap.LEFT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_TYPE,
-                       RobotMap.LEFT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_ADDRESS, 
-                        RobotMap.LEFT_DRIVE_CAN_MOTOR_ISINVERTED),
+	public CanDriveSubsystem() {
 
-                // Right Speed Controller
-                new TCanSpeedController(
-                        RobotMap.RIGHT_DRIVE_CAN_SPEED_CONTROLLER_TYPE,
-                        RobotMap.RIGHT_DRIVE_CAN_SPEED_CONTROLLER_ADDRESS,
-                       RobotMap.RIGHT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_TYPE,
-                       RobotMap.RIGHT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_ADDRESS, 
-                        RobotMap.RIGHT_DRIVE_CAN_MOTOR_ISINVERTED),
+		super(
+				// Left Speed Controller
+				new TCanSpeedController(
+						RobotMap.LEFT_DRIVE_CAN_SPEED_CONTROLLER_TYPE,
+						RobotMap.LEFT_DRIVE_CAN_SPEED_CONTROLLER_ADDRESS,
+						RobotMap.LEFT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_TYPE,
+						RobotMap.LEFT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_ADDRESS, 
+						RobotMap.LEFT_DRIVE_CAN_MOTOR_ISINVERTED),
 
-                // Gyro used for this subsystem
-                new TAnalogGyro(RobotMap.GYRO_PORT, RobotMap.GYRO_ISINVERTED),
+				// Right Speed Controller
+				new TCanSpeedController(
+						RobotMap.RIGHT_DRIVE_CAN_SPEED_CONTROLLER_TYPE,
+						RobotMap.RIGHT_DRIVE_CAN_SPEED_CONTROLLER_ADDRESS,
+						RobotMap.RIGHT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_TYPE,
+						RobotMap.RIGHT_DRIVE_CAN_FOLLOWER_SPEED_CONTROLLER_ADDRESS, 
+						RobotMap.RIGHT_DRIVE_CAN_MOTOR_ISINVERTED),
 
-                // Gyro PID Constants
-                RobotConst.DRIVE_GYRO_PID_KP, 
-                RobotConst.DRIVE_GYRO_PID_KI, 
-                RobotConst.DRIVE_MAX_ROTATION_OUTPUT);
+				// Gyro used for this subsystem
+				new TNavXGyro(),
 
-        // Get the encoders attached to the CAN bus speed controllers
-        TEncoder leftEncoder = getSpeedController(TSide.LEFT).getEncoder();
-        TEncoder rightEncoder = getSpeedController(TSide.RIGHT).getEncoder();
+				// Gyro PID Constants
+				RobotConst.DRIVE_GYRO_PID_KP, 
+				RobotConst.DRIVE_GYRO_PID_KI, 
+				RobotConst.DRIVE_MAX_ROTATION_OUTPUT);
 
-        super.setEncoders(
-                leftEncoder,  RobotMap.LEFT_DRIVE_CAN_MOTOR_ISINVERTED,
-                rightEncoder, RobotMap.RIGHT_DRIVE_CAN_MOTOR_ISINVERTED, 
-                RobotConst.ENCODER_COUNTS_PER_INCH, 
-                RobotConst.DRIVE_SPEED_PID_KP,
-                RobotConst.DRIVE_SPEED_PID_KI,
-                RobotConst.MAX_LOW_GEAR_SPEED);
-    }
+		// Get the encoders attached to the CAN bus speed controllers
+		TEncoder leftEncoder = getSpeedController(TSide.LEFT).getEncoder();
+		TEncoder rightEncoder = getSpeedController(TSide.RIGHT).getEncoder();
+
+		super.setEncoders(
+				leftEncoder,  RobotMap.LEFT_DRIVE_CAN_MOTOR_ISINVERTED,
+				rightEncoder, RobotMap.RIGHT_DRIVE_CAN_MOTOR_ISINVERTED, 
+				RobotConst.ENCODER_COUNTS_PER_INCH, 
+				RobotConst.DRIVE_SPEED_PID_KP,
+				RobotConst.DRIVE_SPEED_PID_KI,
+				RobotConst.MAX_LOW_GEAR_SPEED);
+	}
 
 	@Override
 	public void init() {
 		// Put any subsystem initialization code here.
-    	ultrasonicSensor.calibrate(
-    			RobotConst.ULTRASONIC_VOLTAGE_20IN, 
-    			RobotConst.ULTRASONIC_VOLTAGE_40IN, 
-    			RobotConst.ULTRASONIC_VOLTAGE_80IN);
+		ultrasonicSensor.calibrate(
+				RobotConst.ULTRASONIC_VOLTAGE_20IN, 
+				RobotConst.ULTRASONIC_VOLTAGE_40IN, 
+				RobotConst.ULTRASONIC_VOLTAGE_80IN);
 	}
-	
-    // Initialize the default command for the Chassis subsystem.
-    @Override
-    public void initDefaultCommand() {
-        setDefaultCommand(new DefaultDriveCommand());
-    }
 
-    double leftRampSetpoint = 0;
-    double rightRampSetpoint = 0;
-    double leftCurSpeed = 0;
-    double rightCurSpeed = 0;
+	// Initialize the default command for the Chassis subsystem.
+	@Override
+	public void initDefaultCommand() {
+		setDefaultCommand(new DefaultDriveCommand());
+	}
 
-    // @Override
-    // public void setSpeed(double leftSpeed, double rightSpeed) {
-    //     leftRampSetpoint = leftSpeed;
-    //     rightRampSetpoint = rightSpeed;
-    // }
+	double leftRampSetpoint = 0;
+	double rightRampSetpoint = 0;
+	double leftCurSpeed = 0;
+	double rightCurSpeed = 0;
 
-    public double getUltrasonicDistance() {
-    	return ultrasonicSensor.getDistance();
-    }
-    
-    @Override 
-    public void updatePeriodic() {
+	// @Override
+	// public void setSpeed(double leftSpeed, double rightSpeed) {
+	//     leftRampSetpoint = leftSpeed;
+	//     rightRampSetpoint = rightSpeed;
+	// }
 
-    	super.updatePeriodic();
+	public double getUltrasonicDistance() {
+		return ultrasonicSensor.getDistance();
+	}
 
-    	SmartDashboard.putNumber("Ultrasonic Voltage",   ultrasonicSensor.getRawVoltage());
-    	SmartDashboard.putNumber("Ultrasonic Distance" , getUltrasonicDistance());
+	@Override 
+	public void updatePeriodic() {
 
-    }
+		super.updatePeriodic();
 
-    //     // Ramp toward the setpoint
-    //     if (Math.abs(leftCurSpeed - leftRampSetpoint) < .2) {
-    //         leftCurSpeed = leftRampSetpoint;
-    //     }
-    //     else {
-    //         if (leftCurSpeed < leftRampSetpoint)  {
-    //             leftCurSpeed += .200;
-    //         }
-    //         if (leftCurSpeed < leftRampSetpoint) {
-    //             leftCurSpeed -= .200;
-    //         }
-    //     }
+		SmartDashboard.putNumber("Ultrasonic Voltage",   ultrasonicSensor.getRawVoltage());
+		SmartDashboard.putNumber("Ultrasonic Distance" , getUltrasonicDistance());
 
-    //     if (Math.abs(rightCurSpeed - rightRampSetpoint) < .2) {
-    //         rightCurSpeed = rightRampSetpoint;
-    //     }
-    //     else {
-    //         if (rightCurSpeed < rightRampSetpoint)  {
-    //             rightCurSpeed += .200;
-    //         }
-    //         if (rightCurSpeed < rightRampSetpoint) {
-    //             rightCurSpeed -= .200;
-    //         }
-    //     }
+	}
 
-    //     super.setSpeed(leftCurSpeed, rightCurSpeed);
+	//     // Ramp toward the setpoint
+	//     if (Math.abs(leftCurSpeed - leftRampSetpoint) < .2) {
+	//         leftCurSpeed = leftRampSetpoint;
+	//     }
+	//     else {
+	//         if (leftCurSpeed < leftRampSetpoint)  {
+	//             leftCurSpeed += .200;
+	//         }
+	//         if (leftCurSpeed < leftRampSetpoint) {
+	//             leftCurSpeed -= .200;
+	//         }
+	//     }
 
-    //     super.updatePeriodic();
-    // }
+	//     if (Math.abs(rightCurSpeed - rightRampSetpoint) < .2) {
+	//         rightCurSpeed = rightRampSetpoint;
+	//     }
+	//     else {
+	//         if (rightCurSpeed < rightRampSetpoint)  {
+	//             rightCurSpeed += .200;
+	//         }
+	//         if (rightCurSpeed < rightRampSetpoint) {
+	//             rightCurSpeed -= .200;
+	//         }
+	//     }
+
+	//     super.setSpeed(leftCurSpeed, rightCurSpeed);
+
+	//     super.updatePeriodic();
+	// }
 
 }
